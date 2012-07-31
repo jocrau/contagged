5b415ad0238d5e5d2b0dff784e99e0223be36357
jocrau <jocrau@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-07-31 19:14:50 +0000

    Deloyed version 1.7.0 to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/contagged/trunk@64977 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,21 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "contagged".
-#
-# Auto generated 06-02-2010 10:44
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+* Extension Manager/Repository config file for ext "contagged".
+*
+* Auto generated 31-07-2012 15:10
+*
+* Manual updates:
+* Only the data in the array - everything else is removed by next
+* writing. "version" and "dependencies" must not be touched!
+***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Content parser and tagger (Glossary)',
 	'description' => 'This extension parses your content to tag, replace and link specific terms. It is useful to auto-generate a glossary - but not only. See \'ChangeLog\' and WiKi (\'http://wiki.typo3.org/index.php/Contagged\').',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.6.5',
+	'version' => '1.7.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:24:{s:9:"ChangeLog";s:4:"01e3";s:10:"README.txt";s:4:"83a1";s:22:"class.tx_contagged.php";s:4:"73ec";s:21:"ext_conf_template.txt";s:4:"d179";s:12:"ext_icon.gif";s:4:"50a3";s:17:"ext_localconf.php";s:4:"0162";s:14:"ext_tables.php";s:4:"36c3";s:14:"ext_tables.sql";s:4:"0afa";s:27:"icon_tx_contagged_terms.gif";s:4:"50a3";s:30:"icon_tx_contagged_terms__h.gif";s:4:"930e";s:16:"locallang_db.xml";s:4:"0305";s:7:"tca.php";s:4:"1d36";s:23:"doc/cooluri_example.xml";s:4:"e0a4";s:14:"doc/manual.sxw";s:4:"8abc";s:23:"doc/realurl_example.php";s:4:"bf01";s:23:"javascript/contagged.js";s:4:"d4f6";s:41:"model/class.tx_contagged_model_mapper.php";s:4:"e3e2";s:40:"model/class.tx_contagged_model_terms.php";s:4:"8890";s:30:"pi1/class.tx_contagged_pi1.php";s:4:"2e70";s:18:"pi1/contagged.tmpl";s:4:"86b9";s:17:"pi1/locallang.xml";s:4:"bcfe";s:20:"static/constants.txt";s:4:"afcb";s:16:"static/setup.txt";s:4:"7ac9";s:25:"static/examples/setup.txt";s:4:"4e66";}',
+	'_md5_values_when_last_written' => 'a:25:{s:9:"ChangeLog";s:4:"ea2d";s:22:"class.tx_contagged.php";s:4:"e3df";s:21:"ext_conf_template.txt";s:4:"d179";s:12:"ext_icon.gif";s:4:"50a3";s:17:"ext_localconf.php";s:4:"0f7c";s:14:"ext_tables.php";s:4:"55a2";s:14:"ext_tables.sql";s:4:"0cf0";s:27:"icon_tx_contagged_terms.gif";s:4:"50a3";s:30:"icon_tx_contagged_terms__h.gif";s:4:"930e";s:16:"locallang_db.xml";s:4:"f99a";s:10:"README.txt";s:4:"83a1";s:7:"tca.php";s:4:"5b26";s:29:"tx_contagged_userfunction.php";s:4:"a573";s:23:"doc/cooluri_example.xml";s:4:"dc56";s:14:"doc/manual.sxw";s:4:"8abc";s:23:"doc/realurl_example.php";s:4:"8d84";s:23:"javascript/contagged.js";s:4:"d4f6";s:41:"model/class.tx_contagged_model_mapper.php";s:4:"6cb8";s:40:"model/class.tx_contagged_model_terms.php";s:4:"20e0";s:30:"pi1/class.tx_contagged_pi1.php";s:4:"2a63";s:18:"pi1/contagged.tmpl";s:4:"cd71";s:17:"pi1/locallang.xml";s:4:"0aa2";s:20:"static/constants.txt";s:4:"86c2";s:16:"static/setup.txt";s:4:"bc39";s:25:"static/examples/setup.txt";s:4:"d815";}',
 	'suggests' => array(
 	),
 );
